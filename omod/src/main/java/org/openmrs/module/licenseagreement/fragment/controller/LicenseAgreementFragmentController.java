7b60719fa380a0ef415b4c244d30daa545190ad7
Ivange Larry <ivangelarry@gmail.com>
2018-06-30 12:24:39 +0100

    delete unused imports

--- omod/src/main/java/org/openmrs/module/licenseagreement/fragment/controller/LicenseAgreementFragmentController.java
+++ omod/src/main/java/org/openmrs/module/licenseagreement/fragment/controller/LicenseAgreementFragmentController.java
@@ -2,20 +2,15 @@ package org.openmrs.module.licenseagreement.fragment.controller;
 
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.licenseagreement.LicensedUser;
 import org.openmrs.module.licenseagreement.api.LicenseAgreementModuleService;
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
-import org.openmrs.ui.framework.fragment.FragmentRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 public class LicenseAgreementFragmentController {
-	
+
 	private LicenseAgreementModuleService service = Context.getService(LicenseAgreementModuleService.class);
-	
+
 	public SimpleObject acceptLicenseAgreement(@RequestParam("action") String action, UiUtils uiUtils) {
 		User user = Context.getAuthenticatedUser();
 		SimpleObject response = new SimpleObject();
@@ -28,7 +23,7 @@ public class LicenseAgreementFragmentController {
 		}
 		return response;
 	}
-	
+
 	public SimpleObject getLicenseAgreement() {
 		String licenseBody = service.getLicenseAgreement().getBody();
 		SimpleObject response = new SimpleObject();
